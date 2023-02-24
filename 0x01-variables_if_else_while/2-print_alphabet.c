fedd3373071ac0cb0cca7c834473b551ff85dec7
papisamgh <papisamgh@gmail.com>
2023-02-23 20:43:27 -0800

    if else variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabets
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char alp[26] - "abcdefghihjklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i];
+	}
+	putchar('\n');
+	return (0);
+}
